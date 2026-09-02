398c54a097c83aa09b9c13828f4b60e2c46fd834
Anang <anang2804@users.noreply.github.com>
2026-09-02 17:30:59 +0700

    modified:   cypress/e2e/loginPage.cy.js

--- cypress/e2e/loginPage.cy.js
+++ cypress/e2e/loginPage.cy.js
@@ -6,14 +6,14 @@ describe("TC_LOGIN Pengguna dapat login dengan valid kredensial", () => {
     loginPage.visitUrl();
   });
 
-  it("TC-001 Login dengan username terdaftar dan password yang benar", () => {
+  it("TC_LOG_001 Login dengan username terdaftar dan password yang benar", () => {
     loginPage.inputUsername(loginData.validUsername);
     loginPage.inputPassword(loginData.validPassword);
     loginPage.clickLoginBtn();
     loginPage.dashboardUrlValidation();
   });
 
-  it("TC-002 Login dengan username yang valid dan password invalid", () => {
+  it("TC_LOG_002 Login dengan username yang valid dan password invalid", () => {
     loginPage.inputUsername(loginData.validUsername);
     loginPage.inputPassword(loginData.invalidPassword);
     loginPage.clickLoginBtn();
@@ -21,7 +21,7 @@ describe("TC_LOGIN Pengguna dapat login dengan valid kredensial", () => {
     loginPage.loginUrlValidation();
   });
 
-  it("TC-003 Login dengan username invalid dan password yang valid", () => {
+  it("TC_LOG_003 Login dengan username invalid dan password yang valid", () => {
     loginPage.inputUsername(loginData.invalidUsername);
     loginPage.inputPassword(loginData.validPassword);
     loginPage.clickLoginBtn();
@@ -29,48 +29,48 @@ describe("TC_LOGIN Pengguna dapat login dengan valid kredensial", () => {
     loginPage.loginUrlValidation();
   });
 
-  it("TC-004 Login dengan password dan empty username", () => {
+  it("TC_LOG_004 Login dengan password dan empty username", () => {
     loginPage.inputPassword(loginData.validPassword);
     loginPage.clickLoginBtn();
     loginPage.requiredFieldValidation(loginData.requiredAlert);
     loginPage.loginUrlValidation();
   });
 
-  it("TC-005 Login dengan username dan empty password", () => {
+  it("TC_LOG_005 Login dengan username dan empty password", () => {
     loginPage.inputUsername(loginData.validUsername);
     loginPage.clickLoginBtn();
     loginPage.requiredFieldValidation(loginData.requiredAlert);
     loginPage.loginUrlValidation();
   });
 
-  it("TC-006 Login dengan empty username dan password", () => {
+  it("TC_LOG_006 Login dengan empty username dan password", () => {
     loginPage.clickLoginBtn();
     loginPage.bothAlert();
     loginPage.loginUrlValidation();
   });
 
-  it("TC-007 Login dengan username case sensitive", () => {
+  it("TC_LOG_007 Login dengan username case sensitive", () => {
     loginPage.inputUsername(loginData.lowerCaseUsername);
     loginPage.inputPassword(loginData.validPassword);
     loginPage.clickLoginBtn();
     loginPage.dashboardUrlValidation();
   });
 
-  it("TC-008 Klik link 'Forgot your password?' menuju halaman Reset Password", () => {
+  it("TC_LOG_008 Klik link 'Forgot your password?' menuju halaman Reset Password", () => {
     loginPage.clickForgotPassword();
     loginPage.forgotPassUrlValidation();
     loginPage.usernameFieldVisible();
     loginPage.resetPasswordBtnVisible();
   });
 
-  it("TC-009 Submit form Forgot Password dengan field username dikosongkan", () => {
+  it("TC_LOG_009 Submit form Forgot Password dengan field username dikosongkan", () => {
     loginPage.clickForgotPassword();
     loginPage.clickResetPasswordBtn();
     loginPage.requiredFieldValidation(loginData.requiredAlert);
     loginPage.forgotPassUrlValidation();
   });
 
-  it("TC-010 Submit form Forgot Password dengan username terdaftar", () => {
+  it("TC_LOG_010 Submit form Forgot Password dengan username terdaftar", () => {
     loginPage.clickForgotPassword();
     loginPage.inputUsername(loginData.validUsername);
     loginPage.clickResetPasswordBtn();
